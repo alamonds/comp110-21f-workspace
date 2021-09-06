d7c0d932eee503a9c608cbe8ac76951c5e1a6ee0
Alyssa Lamonds <alamonds2live.unc.edu>
2021-09-06 18:05:29 -0400

    finished ex02

---
+++ exercises 3/ex02/count_letters.py
@@ -0,0 +1,16 @@
+"""Counting letters in a string."""
+
+__author__ = "730345034"
+
+
+# Begin your solution here...
+letter: str = str(input("What letter do you want to search for? "))
+word: str = str(input("Enter a word: "))
+counter: int = 0
+result: int = 0
+word_length: int = len(word)
+while counter < word_length:
+    if word[counter] == letter:
+        result = result + 1
+    counter = counter + 1
+print("Count: " + str(result)) 
---
+++ exercises 3/ex02/fortune_cookie.py
@@ -0,0 +1,29 @@
+"""Program that outputs one of at least four random, good fortunes."""
+
+__author__ = "730345034"
+
+# The randint function is imported from the random library so that
+# you are able to generate integers at random.
+# 
+# Documentation: https://docs.python.org/3/library/random.html#random.randint
+#
+# For example, consider the function call expression: randint(1, 100)
+# It will evaluate to an int value >= 1 and <= 100. 
+from random import randint
+
+
+# Begin your solution here...
+FORTUNE: int = 5
+random: int = randint(1,10)
+print("Your fortune cookie says... ")
+if random == FORTUNE:
+    print("You will get an A on your next exam.")
+else:
+    print("You will have a great day!")
+    if random > FORTUNE: 
+        print("Something exciting is coming!")
+    else:
+        print("Someone special is coming your way!") 
+
+print("Now, go spread positive vibes!")   
+
